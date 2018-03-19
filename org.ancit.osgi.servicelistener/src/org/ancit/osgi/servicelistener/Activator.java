a10b1e2ccc1a9e685bb635eb2f96e6e247c2c371
annamalai <annamalai.ancitconsulting.com>
2018-03-20 01:17:11 +0530

    OSGI ServiceListener

---
+++ org.ancit.osgi.servicelistener/src/org/ancit/osgi/servicelistener/Activator.java
@@ -0,0 +1,39 @@
+package org.ancit.osgi.servicelistener;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+
+public class Activator implements BundleActivator, ServiceListener {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		context.addServiceListener(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		context.removeServiceListener(this);
+		Activator.context = null;
+	}
+
+	@Override
+	public void serviceChanged(ServiceEvent event) {
+		System.out.println(event);
+	}
+
+}

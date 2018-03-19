4e18c78ce9d855d0242427bcde58a734f1e5e7e8
annamalai <annamalai.ancitconsulting.com>
2018-03-19 08:13:29 +0530

    Hello World OSGI Service Bundle

---
+++ org.ancit.osgi.helloservice/src/org/ancit/osgi/helloservice/Activator.java
@@ -0,0 +1,32 @@
+package org.ancit.osgi.helloservice;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		System.out.println("Bundle Started : HelloWorld");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("Bundle Stopped : HelloWorld");
+		Activator.context = null;
+	}
+
+}
